b445a03858ece23841780efd3a61ce1cb7680d79
Danil Mikhailov <danil.present@mail.ru>
2021-10-03 19:34:03 +0300

    [MASTER] init db conn and endpoints

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,60 @@
 package main
 
 import (
+	"flag"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"mygame/config"
+	"mygame/dependers/database"
+	"mygame/internal/endpoint"
 	"net/http"
 	"path/filepath"
 	"strconv"
 )
 
+const defaultPacksPath = "./packs"
+
+var packsPath string
+
+func init() {
+	flag.StringVar(&packsPath, "packs-path", defaultPacksPath, "packs path")
+	flag.StringVar(&packsPath, "p", defaultPacksPath, "packs path")
+}
+
 func main() {
+	flag.Parse()
+
 	config, err := parseCfg("./config/config.yaml")
-	if err != nil {
+	if err != nil{
 		panic(err)
 	}
 
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(config.App.Port), nil))
+	config.Pack.Path = packsPath
+
+	connectionAddr := &database.Connection{
+		Host:     config.DB.Host,
+		Port:     config.DB.Port,
+		User:     config.DB.User,
+		Password: config.DB.Password,
+		DBName:   config.DB.DBName,
+		SSLMode:  config.DB.SSLMode,
+	}
+
+	connectionAddrStr := database.GenerateAddr(connectionAddr)
+
+	db, err := database.NewDB(connectionAddrStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	endpoint := endpoint.NewEndpoint(db, config)
+	endpoint.InitRoutes()
+
+	log.Fatal(http.ListenAndServe(":" + strconv.Itoa(config.App.Port), nil))
 }
 
-func parseCfg(path string) (*config.Config, error) {
+func parseCfg(path string) (*config.Config, error){
 	filename, _ := filepath.Abs(path)
 	yamlFile, err := ioutil.ReadFile(filename)
 
--- config/config.go
+++ config/config.go
@@ -3,9 +3,10 @@ package config
 import "time"
 
 type Config struct {
-	App App `yaml:"app"`
-	DB  DB  `yaml:"db"`
-	JWT JWT `yaml:"jwt"`
+	App  App `yaml:"app"`
+	DB   DB  `yaml:"db"`
+	JWT  JWT `yaml:"jwt"`
+	Pack Pack
 }
 
 type App struct {
@@ -24,4 +25,8 @@ type DB struct {
 type JWT struct {
 	SecretKey      string        `yaml:"secret_key"`
 	ExpirationTime time.Duration `yaml:"expiration_time"`
+}
+
+type Pack struct {
+	Path string
 }
