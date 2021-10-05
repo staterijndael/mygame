97d10bbddca85684e24886087b3fbd4eeb313bdb
Danil Mikhailov <danil.present@mail.ru>
2021-10-06 01:35:58 +0300

    [FEATURE/LOGGER-1] add logger and context for logger and request token

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"flag"
+	"go.uber.org/zap"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
 	"mygame/config"
 	"mygame/dependers/database"
+	"mygame/dependers/logger"
 	"mygame/internal/endpoint"
 	"mygame/internal/singleton"
+	"mygame/tools/helpers"
 	"net/http"
 	"path/filepath"
 	"strconv"
@@ -49,10 +52,25 @@ func main() {
 		log.Fatal(err)
 	}
 
+	logger, err := logger.ConfigureLogger(config.App.LogLevel)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logger = logger.With(zap.String("app_token", helpers.GenerateRandomString(16)))
+
 	singleton.InitSingleton()
 
-	endpoint := endpoint.NewEndpoint(db, config)
+	endpoint := endpoint.NewEndpoint(db, config, logger)
 	endpoint.InitRoutes()
+
+	logger.Info(
+		"My game server started",
+		zap.Int("port", config.App.Port),
+		zap.String("log_level", config.App.LogLevel),
+		zap.String("database_name", config.DB.DBName),
+		zap.String("database_host", config.DB.Host+":"+config.DB.Port),
+	)
 
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(config.App.Port), nil))
 }
--- go.mod
+++ go.mod
@@ -4,9 +4,11 @@ go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/gen2brain/go-unarr v0.1.1 // indirect
 	github.com/gorilla/websocket v1.4.2
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/lib/pq v1.2.0
+	go.uber.org/zap v1.19.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
